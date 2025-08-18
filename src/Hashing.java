6b5fd90349b2d3d7be3c6a6fce1a851d0d688d76
B-Shankar <learn.bhimashankar@gmail.com>
2025-08-18 22:11:06 +0530

    hashing

---
+++ src/Hashing.java
@@ -0,0 +1,9 @@
+public class Hashing {
+    public static void main(String[] args) {
+        String name = "Kunal";
+        Integer a = 72858447;
+        int value = name.hashCode();
+
+        System.out.println("Hash code of " + name + " is: " + value);
+    }
+}
